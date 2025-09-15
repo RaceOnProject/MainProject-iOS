259c6ae27bd1850290825d35db138affd1668c87
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-09-15 20:48:59 +0900

    build: 프로젝트 생성

---
+++ FlowFolio/FlowFolio/FlowFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFolioApp.swift
+//  FlowFolio
+//
+//  Created by 조성민 on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
