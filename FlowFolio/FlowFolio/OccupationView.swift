256e3240cfbd99b6c554323c24bfe894f58d4eb1
황동혁 <ghkdehdgur01@gmail.com>
2025-09-27 11:29:23 +0900

    OccupationView 추가

---
+++ FlowFolio/FlowFolio/OccupationView.swift
@@ -0,0 +1,20 @@
+//
+//  OccupationView.swift
+//  FlowFolio
+//
+//  Created by 황동혁 on 9/27/25.
+//
+
+import SwiftUI
+
+struct OccupationView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    OccupationView()
+}
