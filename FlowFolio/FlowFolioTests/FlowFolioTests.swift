84f5e451acc6f61db2f36c2520813d85c7132706
woong <workplayhard1@naver.com>
2025-09-29 14:05:04 +0900

    feat: SwiftTests 타겟 추가

---
+++ FlowFolio/FlowFolioTests/FlowFolioTests.swift
@@ -0,0 +1,18 @@
+//
+//  FlowFolioTests.swift
+//  FlowFolioTests
+//
+//  Created by KOVI on 9/29/25.
+//
+
+import Testing
+
+struct FlowFolioTests {
+
+    @Test func example() async throws {
+        // Given
+        // When
+        // Then
+    }
+
+}
