36df3daab2dc2eef173a265c701c56c39541ff25
woong <workplayhard1@naver.com>
2025-10-14 14:03:08 +0900

    test: MockURLSession로 테스트되도록 수정구현

--- FlowFolio/FlowFolioTests/NetworkService/NetworkServiceTests.swift
+++ FlowFolio/FlowFolioTests/NetworkService/NetworkServiceTests.swift
@@ -13,25 +13,25 @@ import Foundation
 struct NetworkServiceTests {
     @Test("성공케이스 -(200응답/JSONDATA) 일 때, 메시지를 정상 디코딩")
     func testSuccessfulRequest() async throws {
-        defer { MockURLProtocol.mockResponseHandler = nil }
-        
         // Given
         let expectMessage = "ok"
         let expectJson: Data = #"{"message":"ok"}"#.data(using: .utf8)!
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.protocolClasses = [MockURLProtocol.self]
-        let mockSession = URLSession(configuration: configuration)
-        MockURLProtocol.mockResponseHandler = { request in
-            let response = HTTPURLResponse(
-                url: request.url!,
-                statusCode: 200,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            return (response, expectJson)
-        }
+        let url = URL(string: "https://api.example.com/test")!
+        /// 1. Mock 응답 구성
+        let mockResponse = HTTPURLResponse(
+            url: url,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        /// 2. Mock Session 구성
+        let mockSession: URLSessionProtocol = MockURLSession(
+            data: expectJson,
+            response: mockResponse
+        )
+        /// 3. NetworkService에 Mock Session 주입
         let sut = NetworkService(session: mockSession)
-        let request = URLRequest(url: URL(string: "https://api.example.com/test")!)
+        let request = URLRequest(url: url)
 
         // When
         let result: MockMessageResponse = try await sut.request(request)
@@ -42,26 +42,26 @@ struct NetworkServiceTests {
 
     @Test("실패케이스 -(400 Bad Request) 일 때, NetworkError.serverError(400) 뱉는지 여부")
     func testServerError400() async throws {
-        defer { MockURLProtocol.mockResponseHandler = nil }
-        
         // Given
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.protocolClasses = [MockURLProtocol.self]
-        let mockSession = URLSession(configuration: configuration)
-        MockURLProtocol.mockResponseHandler = { request in
-            let response = HTTPURLResponse(
-                url: request.url!,
-                statusCode: 400,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            return (response, Data())
-        }
+        let url = URL(string: "https://api.example.com/test")!
+        /// 1. Mock 응답 구성
+        let mockResponse = HTTPURLResponse(
+            url: url,
+            statusCode: 400,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        /// 2. Mock Session 구성
+        let mockSession: URLSessionProtocol = MockURLSession(
+            data: Data(),
+            response: mockResponse
+        )
+        /// 3. NetworkService에 Mock Session 주입
         let sut = NetworkService(session: mockSession)
-        let request = URLRequest(url: URL(string: "https://api.example.com/test")!)
+        let request = URLRequest(url: url)
 
-        // When
         do {
+            // When
             let _: Dummy = try await sut.request(request)
             assertionFailure("이쪽 접근되면 안됨")
         } catch let error as NetworkError {
@@ -78,25 +78,24 @@ struct NetworkServiceTests {
 
     @Test("실패케이스 -(디코딩 실패) 일 때, NetworkError.decodingFailed 뱉는지 여부")
     func testDecodingFailure() async throws {
-        defer { MockURLProtocol.mockResponseHandler = nil }
-        
         // Given
         let invalidJsonData = "{ I'M INVALID JSON }".data(using: .utf8)!
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.protocolClasses = [MockURLProtocol.self]
-        let mockSession = URLSession(configuration: configuration)
-        MockURLProtocol.mockResponseHandler = { request in
-            let response = HTTPURLResponse(
-                url: request.url!,
-                statusCode: 200,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            return (response, invalidJsonData)
-        }
-
+        let url = URL(string: "https://api.example.com/user")!
+        /// 1. Mock 응답 구성
+        let mockResponse = HTTPURLResponse(
+            url: url,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        /// 2. Mock Session 구성
+        let mockSession: URLSessionProtocol = MockURLSession(
+            data: invalidJsonData,
+            response: mockResponse
+        )
+        /// 3. NetworkService에 Mock Session 주입
         let sut = NetworkService(session: mockSession)
-        let request = URLRequest(url: URL(string: "https://api.example.com/user")!)
+        let request = URLRequest(url: url)
 
         do {
             // When
