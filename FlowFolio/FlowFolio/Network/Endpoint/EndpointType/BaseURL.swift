8c6b76b2dd4a724789fe2a9c856263337830a2f0
woong <workplayhard1@naver.com>
2025-10-17 16:09:52 +0900

    fix: fatalError를 assertionFailure로 수정

--- FlowFolio/FlowFolio/Network/Endpoint/EndpointType/BaseURL.swift
+++ FlowFolio/FlowFolio/Network/Endpoint/EndpointType/BaseURL.swift
@@ -11,7 +11,8 @@ enum NetworkConfig {
     static let baseURL: URL = {
         guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String,
               let baseURL = URL(string: urlString) else {
-            fatalError("Config의 BaseURL값을 읽어올 수 없음")
+            assertionFailure("Config의 BaseURL값을 읽어올 수 없음")
+            return URL(string: "")!
         }
         return baseURL
     }()
